a96fb30a3a58c317a2c662351e04780b400a9441
Daeyun <sweet_ray@naver.com>
2024-06-13 22:08:00 +0900

    Feat: 런치스크린 UI 구성

---
+++ MeaningOut/Onboarding/Onboarding.swift
@@ -0,0 +1,42 @@
+//
+//  Onboarding.swift
+//  MeaningOut
+//
+//  Created by 권대윤 on 6/13/24.
+//
+
+import UIKit
+
+import SnapKit
+
+final class Onboarding: UIViewController {
+    
+    //MARK: - Properties
+    
+    
+    
+    //MARK: - UI Components
+    
+    
+    
+    //MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureLayout()
+        configureUI()
+    }
+    
+    private func configureLayout() {
+        
+    }
+    
+    private func configureUI() {
+        
+    }
+    
+    
+    //MARK: - Functions
+    
+
+}
--- MeaningOut/ViewController.swift
+++ MeaningOut/ViewController.swift
@@ -7,11 +7,29 @@
 
 import UIKit
 
+import SnapKit
+
 class ViewController: UIViewController {
+    
+    let label = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
+        label.text = "MeaningOut"
+        label.font = UIFont(name: "AlfaSlabOne-Regular", size: 30)
+        
+        view.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(50)
+        }
+        
+        for family in UIFont.familyNames.sorted() {
+            let names = UIFont.fontNames(forFamilyName: family)
+            print("Family: \(family) Font names: \(names)")
+        }
+        
     }
 
 
