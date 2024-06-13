b3ff35c580128d7ed16c455c08a8e85cd09fbb13
Daeyun <sweet_ray@naver.com>
2024-06-14 00:21:32 +0900

    Feat: Constant 파일 생성하여 프로젝트 내 사용할 UIColor와 UIFont 열거형 추가

---
+++ MeaningOut/Utils/Constant.swift
@@ -0,0 +1,26 @@
+//
+//  Constant.swift
+//  MeaningOut
+//
+//  Created by 권대윤 on 6/13/24.
+//
+
+import UIKit
+
+enum Constant {
+    enum Color {
+        static let primaryOrange = UIColor(red: 0.94, green: 0.54, blue: 0.28, alpha: 1.00)
+        static let primaryBlack = UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha: 1.00)
+        static let primaryGray = UIColor(red: 0.51, green: 0.51, blue: 0.51, alpha: 1.00)
+        static let primaryDarkGray = UIColor(red: 0.30, green: 0.30, blue: 0.30, alpha: 1.00)
+        static let primaryLightGray = UIColor(red: 0.80, green: 0.80, blue: 0.80, alpha: 1.00)
+        static let primaryWhite = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.00)
+    }
+    
+    enum Font {
+        static let system16 = UIFont.systemFont(ofSize: 16)
+        static let system15 = UIFont.systemFont(ofSize: 15)
+        static let system14 = UIFont.systemFont(ofSize: 14)
+        static let system13 = UIFont.systemFont(ofSize: 13)
+    }
+}
