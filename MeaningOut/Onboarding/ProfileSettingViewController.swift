ad9c4e72cc4a641948bb6fd2eb44bbf85e9e6a52
Daeyun <sweet_ray@naver.com>
2024-06-14 11:20:01 +0900

    Feat: 프로필 닉네임 설정화면에서 닉네임 조건에 따라 완료 버튼 활성화 및 조건 상태 레이블에 안내 문구 표시 기능 구현

--- MeaningOut/Onboarding/ProfileSettingViewController.swift
+++ MeaningOut/Onboarding/ProfileSettingViewController.swift
@@ -39,13 +39,15 @@ final class ProfileSettingViewController: UIViewController {
         return btn
     }()
     
-    private let nicknameTextField: UITextField = {
+    private lazy var nicknameTextField: UITextField = {
         let tf = UITextField()
-        tf.attributedPlaceholder = NSAttributedString(string: "닉네임을 입력해주세요 )", attributes: [.foregroundColor: Constant.Color.primaryGray, .font: Constant.Font.system15])
+        tf.attributedPlaceholder = NSAttributedString(string: "닉네임을 입력해주세요 :)", attributes: [.foregroundColor: Constant.Color.primaryGray, .font: Constant.Font.system15])
         tf.backgroundColor = .clear
         tf.autocapitalizationType = .none
         tf.autocorrectionType = .no
         tf.keyboardType = .default
+        tf.returnKeyType = .done
+        tf.delegate = self
         return tf
     }()
     
@@ -59,14 +61,14 @@ final class ProfileSettingViewController: UIViewController {
         let label = UILabel()
         label.font = Constant.Font.system13
         label.textColor = Constant.Color.primaryOrange
-        label.text = "닉네임에 @ 는 포함할 수 없어요."
         return label
     }()
     
     private lazy var completeButton: UIButton = {
         let btn = UIButton(type: .system)
         btn.setTitle("완료", for: .normal)
-        btn.backgroundColor = Constant.Color.primaryOrange
+        btn.backgroundColor = Constant.Color.primaryGray
+        btn.isEnabled = false
         btn.layer.cornerRadius = 24
         btn.tintColor = Constant.Color.primaryWhite
         btn.titleLabel?.font = .boldSystemFont(ofSize: 17)
@@ -137,11 +139,62 @@ final class ProfileSettingViewController: UIViewController {
         profileImageView.image = UIImage(named: Constant.ProfileImage.allCases.randomElement()?.rawValue ?? "profile_0")
     }
     
-    
     //MARK: - Functions
     
     @objc func completeButtonTapped() {
         print(#function)
+    }
+    
+    private func updateStatusCompleteButton() {
+        guard let text = nicknameTextField.text else {return}
+        
+        if text.count >= 2 && text.count < 10 && !text.contains("@") && !text.contains("#") && !text.contains("$") && !text.contains("%") && !text.contains("1") && !text.contains("2") && !text.contains("3") && !text.contains("4") && !text.contains("5") && !text.contains("6") && !text.contains("7") && !text.contains("8") && !text.contains("9") && !text.contains("0") {
+            completeButton.isEnabled = true
+            completeButton.backgroundColor = Constant.Color.primaryOrange
+        } else {
+            completeButton.isEnabled = false
+            completeButton.backgroundColor = Constant.Color.primaryGray
+        }
+    }
+    
+    private func updateStatusNicknameConditionLabel() {
+        guard let text = nicknameTextField.text else {return}
+        
+        if text.trimmingCharacters(in: .whitespaces).isEmpty {
+            self.nicknameConditionLabel.text = nil
+            return
+        }
+        
+        if text.contains("@") || text.contains("#") || text.contains("$") || text.contains("%") {
+            self.nicknameConditionLabel.text = "닉네임에 @, #, $, % 는 포함할 수 없어요."
+            return
+        }
+        
+        if text.count < 2 || text.count >= 10 {
+            self.nicknameConditionLabel.text = "2글자 이상 10글자 미만으로 설정해주세요."
+            return
+        } else {
+            if text.contains("1") || text.contains("2") || text.contains("3") || text.contains("4") || text.contains("5") || text.contains("6") || text.contains("7") || text.contains("8") || text.contains("9") || text.contains("0") {
+                self.nicknameConditionLabel.text = "닉네임에 숫자는 포함할 수 없어요."
+                return
+            } else {
+                self.nicknameConditionLabel.text = "사용 가능한 닉네임입니다."
+            }
+        }
+    }
+}
+
+//MARK: - UITextFieldDelegate
+
+extension ProfileSettingViewController: UITextFieldDelegate {
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        self.updateStatusNicknameConditionLabel()
+        self.updateStatusCompleteButton()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return true
     }
 }
 
