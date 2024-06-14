35b9249a242bec87eeea3c150e8554a0af964710
Daeyun <sweet_ray@naver.com>
2024-06-14 14:38:21 +0900

    Feat: 컬렉션뷰에 재사용 식별자 연산 프로퍼티 추가

---
+++ MeaningOut/Utils/Reuseable.swift
@@ -0,0 +1,18 @@
+//
+//  Reuseable.swift
+//  MeaningOut
+//
+//  Created by 권대윤 on 6/14/24.
+//
+
+import UIKit
+
+protocol Reuseable: AnyObject {
+    static var identifier: String {get}
+}
+
+extension UICollectionViewCell: Reuseable {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
