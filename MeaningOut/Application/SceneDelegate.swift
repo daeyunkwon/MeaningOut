dd6b506a3ec7ff7791322025b98d9be1eadb2886
Daeyun <sweet_ray@naver.com>
2024-06-14 15:42:41 +0900

    Feat: 유저 닉네임 데이터가 없는 경우 온보딩 화면부터 앱이 시작되도록 구현

--- MeaningOut/Application/SceneDelegate.swift
+++ MeaningOut/Application/SceneDelegate.swift
@@ -18,7 +18,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = UINavigationController(rootViewController: OnboardingViewController())
+        
+        if UserDefaultsManager.shared.nickname != nil {
+            window?.rootViewController = MainTabBarController()
+        } else {
+            window?.rootViewController = UINavigationController(rootViewController: OnboardingViewController())
+        }
+        
         window?.makeKeyAndVisible()
     }
 
