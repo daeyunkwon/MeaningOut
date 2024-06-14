72896e25fd051a609eea2b8120e2cc615cab9376
Daeyun <sweet_ray@naver.com>
2024-06-14 15:02:21 +0900

    Chore: 리소스 파일 폴더 경로 변경

---
+++ MeaningOut/MainTabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  TabBarController.swift
+//  MeaningOut
+//
+//  Created by 권대윤 on 6/14/24.
+//
+
+import UIKit
+
+final class MainTabBarController: UITabBarController {
+    
+    //MARK: - Properties
+    
+    
+    
+    //MARK: - UI Components
+    
+    
+    
+    //MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    private func setupTabBar() {
+        let mainVC = MainViewController()
+        let
+    }
+    
+    
+    //MARK: - Functions
+    
+
+}
